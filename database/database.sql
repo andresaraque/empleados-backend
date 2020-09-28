5c5e37d4777372bd9d5ff15681b772516e95e61f
andresaraque <andresaraque24dos@gmail.com>
2020-09-28 14:58:08 -0500

    nuevos datos para DB

--- database/database.sql
+++ database/database.sql
@@ -11,9 +11,9 @@ CREATE TABLE ESchema.Employees (
 GO
 
 INSERT INTO ESchema.Employees (Nombre, Salario, Cargo, Img) VALUES
-(N'Jared', 2200000, N'Frontend', N'aquí su imagen'),
-(N'Nikita', 1200000, N'Backend', N'aquí su imagen'),
-(N'Tom', 930500, N'Aseo', N'aquí su imagen');
+(N'Elon Musk', 3600000, N'Desarrollador-fronend', N'https://cdn.vox-cdn.com/thumbor/4QtOwnOxCdwESvt1-CpQSTZvHHA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19932738/1206292068.jpg.jpg'),
+(N'Carlos Carmona', 1200000, N'Vicepresidente', N'https://pbs.twimg.com/profile_images/854424193483583488/BJiy2IFJ_400x400.jpg'),
+(N'Camila Avendaño', 3800500, N'Ingeniera Industrial', N'https://images.unsplash.com/photo-1578489758854-f134a358f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80');
 GO
 
 SELECT * FROM ESchema.Employees;
